6c07c6d840778d1b5038fe8275410dd847faca9b
George Mendonça <george1bsilva@outlook.com>
2021-08-26 23:55:34 -0300

    Initial commit

---
+++ src/main/java/george/tads/testlibrarybookspringwebrest/TestLibraryBookSpringWebRestApplication.java
@@ -0,0 +1,13 @@
+package george.tads.testlibrarybookspringwebrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLibraryBookSpringWebRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestLibraryBookSpringWebRestApplication.class, args);
+    }
+
+}
